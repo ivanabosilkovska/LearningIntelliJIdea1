1e4e365cc46d3ab9a9e887c8cbc9e9d4d75e5666
Ivana <bosilkovska.ivana@gmail.com>
2023-01-30 20:27:01 +0100

    my first commit

---
+++ src/tests/FirstClass.java
@@ -0,0 +1,19 @@
+package tests;
+
+public class FirstClass {
+
+
+
+
+    // tuka se pisuva avtomatski testovi
+
+
+    private int age = 15;
+
+    //this test will e run once BUG-565 is fixed
+    public static void sayHello(String name){
+        // TODO: Add support for multiple users
+        System.out.println("Hello " + name);
+    }
+
+}
---
+++ src/tests/standalone/RegistrationFormTests.java
@@ -0,0 +1,32 @@
+package tests.standalone;
+
+public class RegistrationFormTests {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        sayHello("Emilija");
+
+    }
+
+    String a;
+
+    public int getAge() {
+        return age;
+    }
+
+    // tuka se pisuva avtomatski testovi
+
+    public RegistrationFormTests(int age) {
+        this.age = age;
+    }
+
+    private int age = 15;
+
+    //this test will e run once BUG-565 is fixed
+    public static void sayHello(String name){
+        // TODO: Add support for multiple users
+        System.out.println("Hello " + name);
+    }
+
+}
