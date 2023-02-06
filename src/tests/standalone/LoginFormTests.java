f520f687d793b91321c9724ca46561f4e94cf407
Ivana <bosilkovska.ivana@gmail.com>
2023-02-06 20:19:56 +0100

    Added new class for Login form tests

---
+++ src/tests/standalone/LoginFormTests.java
@@ -0,0 +1,9 @@
+package tests.standalone;
+
+public class LoginFormTests {
+    public static void main(String[] args) {
+
+        System.out.println("Adding new automation tests for Login form");
+
+    }
+}
